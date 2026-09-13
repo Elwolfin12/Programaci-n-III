074672e526b02e55150623e5c9c1957f845be086
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-13 22:46:21 +0000

    Coloco y trabajo con los 30 numeros de forma random

--- Taller1/Punto7.java
+++ Taller1/Punto7.java
@@ -5,23 +5,50 @@ public class Punto7 {
     // Vamos a crear el arreglo A con 10 elementos fijos
         int[] a = {12, 7, 8, 3, 20, 15, 6, 9, 14, 1};
 
-        int contarPares = 0;
-        int contarImpares = 0;
+       int contarPares = 0;
+       int contarImpares = 0;
  
-        // Primero contamos cuántos pares e impares hay para poder
         for (int i = 0; i < a.length; i++) {
-            contarPares += (a[i] % 2 == 0) ? 1 : 0;
-            contarImpares += (a[i] % 2 != 0) ? 1 : 0;
+            if (a[i] % 2 == 0) {
+                contarPares++;
+            } else {
+                contarImpares++;
+            }
         }
-
+ 
         // Creamos los arreglos con el operador new, usando los tamaños calculados
         int[] pares = new int[contarPares];
         int[] impares = new int[contarImpares];
  
         int p = 0;
         int im = 0;
+ 
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                pares[p] = a[i];
+                p++;
+            } else {
+                impares[im] = a[i];
+                im++;
+            } 
+        }
 
+        //Muestro los resuldados el /n/n es para que queden bien organizados
+
+          System.out.println("Arreglo original A:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+ 
+        System.out.println("\n\nArreglo de numeros PARES:");
+        for (int i = 0; i < pares.length; i++) {
+            System.out.print(pares[i] + " ");
+        }
+ 
+        System.out.println("\n\nArreglo de numeros IMPARES:");
+        for (int i = 0; i < impares.length; i++) {
+            System.out.print(impares[i] + " ");
+        }
+    }
 
   }
-
-}
---
+++ Taller1/Punto8.java
@@ -0,0 +1,18 @@
+public class Punto8 {
+ 
+    public static void main(String[] args) {
+ 
+    // Como el enunciado pide trabajar con 30 numeros, los llenamos de forma aleatoria usando Math.random(), entre min y max osea minimo y maximo
+        int max = 100;
+        int min = 1;
+ 
+        int[] a = new int[30];
+ 
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * (max - min + 1)) + min;
+        }
+ 
+        int mayor = a[0];
+        int menor = a[0];
+
+        };
