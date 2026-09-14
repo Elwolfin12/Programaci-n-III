c3d7f525762859f32cb8a8fa60af7c89e67c8d92
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-14 02:44:30 +0000

    Luego de calcular la media y guardar los valores mayores y menores y despues muestro los arreglos

--- Taller1/Punto13.java
+++ Taller1/Punto13.java
@@ -4,7 +4,7 @@ public class Punto13 {
     public static void main(String[] args) {
 
         // Arreglo A con numeros aleatorios de 1 a 10
-        
+
         int[] a = new int[10];
 
         for (int i = 0; i < a.length; i++) {
@@ -20,5 +20,68 @@ public class Punto13 {
 
         double media = (double) suma / a.length;
 
+
+
+          // Cuento cuantos son mayores y menores que la media
+        int contarMayores = 0;
+        int contarMenores = 0;
+
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] > media) {
+                contarMayores++;
+            }
+
+            if (a[i] < media) {
+                contarMenores++;
+            }
+        }
+
+        int[] mayores = new int[contarMayores];
+        int[] menores = new int[contarMenores];
+
+        int ma = 0;
+        int men = 0;
+
+        // Guardo los mayores y menores
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] > media) {
+                mayores[ma] = a[i];
+                ma++;
+            }
+
+            if (a[i] < media) {
+                menores[men] = a[i];
+                men++;
+            }
+        }
+
+
+        // Muestro arreglo A
+        System.out.println("Arreglo A:");
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        // Muestro media
+        System.out.println("\n\nMedia (promedio): " + media);
+
+        // Muestro mayores
+        System.out.println("\nElementos MAYORES que la media:");
+
+        for (int i = 0; i < mayores.length; i++) {
+            System.out.print(mayores[i] + " ");
+        }
+
+        // Muerstro menores
+        System.out.println("\n\nElementos MENORES que la media:");
+
+        for (int i = 0; i < menores.length; i++) {
+            System.out.print(menores[i] + " ");
+        }
+
+    }
 
 }
