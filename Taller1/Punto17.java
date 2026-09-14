7f517ebd318f0dc78adf20eb0bd81cb85aee88c4
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-14 18:49:10 +0000

    Creo una matriz con usando new y le doy valores en este caso una matriz 3x3

---
+++ Taller1/Punto17.java
@@ -0,0 +1,17 @@
+public class Punto17 {
+    
+     public static void main(String[] args) {
+ 
+        // Creo la matriz usando el operaddor new y la llenamos directamente convalores fijos siendo una matrz 3x3
+
+        int[][] matriz = new int[3][3];
+
+        matriz[0] = new int[]{1, 2, 3};
+        matriz[1] = new int[]{4, 5, 6};
+        matriz[2] = new int[]{7, 8, 9};
+ 
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+
+
+}
