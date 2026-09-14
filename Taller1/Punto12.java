034d6c434df7617bf25ed030e16478c0fe66fb16
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-14 01:27:16 +0000

    Uso math.random y creo para la operacion para hallar los valores buscados

--- Taller1/Punto12.java
+++ Taller1/Punto12.java
@@ -1,3 +1,25 @@
 public class Punto12 {
-    
+
+      public static void main(String[] args) {
+
+        // Arreglo A con numeros aleatorios de 1 a 5
+        int[] a = new int[10];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int)(Math.random() * 5) + 1;
+        }
+
+        // Valor que estamos buscando
+        int x = (int)(Math.random() * 5) + 1;
+
+        // Contamos cuantas veces aparece x
+        int contador = 0;
+
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] == x) {
+                contador++;
+            }
+        }
+
 }
