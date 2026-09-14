2a48e4329e50484a38dfbbce77f07326891bb884
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-14 19:26:44 +0000

    La cambio de manera mas sencilla ya que un amigo Jhon marin me explico una forma mas breve de hacerla sin tanta vaiana en este caso use variables

--- Taller1/Punto17.java
+++ Taller1/Punto17.java
@@ -2,24 +2,21 @@ public class Punto17 {
     
      public static void main(String[] args) {
  
-        // Creo la matriz usando el operaddor new y la llenamos directamente convalores fijos siendo una matrz 3x3
+        // Creo la matriz con valores fijos con dos dimensiones (filas y columnas)
 
-        int[][] matriz = new int[3][3];
-
-        matriz[0] = new int[]{1, 2, 3};
-        matriz[1] = new int[]{4, 5, 6};
-        matriz[2] = new int[]{7, 8, 9};
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
  
         int filas = matriz.length;
         int columnas = matriz[0].length;
-
-
-        // Creo los arreglos para guardar la suma de cada fila y de cada columna
+ 
+        // creo los arreglos para guardar la suma de cada fila y la suma de cada columna
         int[] sumaFilas = new int[filas];
  
         int[] sumaColumnas = new int[columnas];
-
-
  
         System.out.println("Matriz:");
         for (int i = 0; i < filas; i++) {
@@ -30,7 +27,7 @@ public class Punto17 {
             }
             System.out.println();
         }
-
+ 
         System.out.println("\nSuma de cada fila:");
         for (int i = 0; i < sumaFilas.length; i++) {
             System.out.println("Fila " + (i + 1) + ": " + sumaFilas[i]);
@@ -40,8 +37,8 @@ public class Punto17 {
         for (int j = 0; j < sumaColumnas.length; j++) {
             System.out.println("Columna " + (j + 1) + ": " + sumaColumnas[j]);
         }
- 
     }
+
 
 
 }
